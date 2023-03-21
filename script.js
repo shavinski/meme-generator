54c10e42796058aa74109a1672067683fc2607c6
Jakob <shavinski.jakob@gmail.com>
2023-03-21 13:16:57 -0700

    trying to get my uploaded image as div

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-const image = document.querySelector('#img-url');
-const topText = document.querySelector('#top-text');
-const botText = document.querySelector('#bot-text');
-const submit = document.querySelector('#submit')
+const imageInput = document.querySelector('#img-url');
+const topTextInput = document.querySelector('#top-text');
+const botTextInput = document.querySelector('#bot-text');
+const form = document.querySelector('form')
 const memeContainer = document.querySelector('.created-memes-container')
-const newDiv = document.createElement("div");
+
+// console.log(image.value);
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let div = document.createElement('div');
+    let img = document.createElement('img');
+
+    img.id = 'imgId';
+    img.src = imageInput.value;
+
+    memeContainer.append(div);
+    div.append(img)
+    div.classList.toggle('meme')
+
+    console.log(img.src.height);
+
+    imageInput.value = '';
+    topTextInput.value = '';
+    botTextInput.value = '';
+
+    // div.style.cssText = `height: ${img.naturalHeight}; width: ${img.naturalWidth}`
 
 
 
 
-submit.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('clicked submit', e);
+
+
+    // console.log(event, imageInput.value);
+
+    // let img = document.querySelector('image')
+    // const meme = document.createElement('div');
+    // const memeTopText = document.createElement('div');
+    // const memeBotText = document.createElement('div');
+
+    // meme.classList.toggle('meme');
+    // meme.style.cssText = `background-image:url(${image.value}; height: ${image.clientHeight}; width: ${image.clientWidth}`
+    // memeContainer.append(meme);
+
+    // meme.append(memeBotText, memeTopText);
 });
